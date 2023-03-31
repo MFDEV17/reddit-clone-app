9f962f96ca44fb7a3303d8f7c41370da5aa45b9d
mfdev17 <maximsushkin02@gmail.com>
2023-03-31 16:57:54 +0300

    init project

---
+++ src/main/java/com/mfdev/redditcloneapp/RedditCloneAppApplication.java
@@ -0,0 +1,13 @@
+package com.mfdev.redditcloneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditCloneAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mfdev/redditcloneapp/RedditCloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfdev.redditcloneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
