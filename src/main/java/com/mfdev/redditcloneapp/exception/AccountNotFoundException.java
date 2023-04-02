22796f9b9a0d54e55b42c9fa9e047ac9f93d79ab
mfdev17 <maximsushkin02@gmail.com>
2023-04-03 00:31:35 +0300

    added exception

---
+++ src/main/java/com/mfdev/redditcloneapp/exception/AccountNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mfdev.redditcloneapp.exception;
+
+public class AccountNotFoundException extends RuntimeException {
+
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
