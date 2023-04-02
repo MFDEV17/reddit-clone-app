24046447a43612f28c24e15046a3f903afca2083
mfdev17 <maximsushkin02@gmail.com>
2023-04-03 00:32:56 +0300

    Added custom exception

--- src/main/java/com/mfdev/redditcloneapp/config/security/userdetails/UserDetailsServiceImpl.java
+++ src/main/java/com/mfdev/redditcloneapp/config/security/userdetails/UserDetailsServiceImpl.java
@@ -1,11 +1,14 @@
 package com.mfdev.redditcloneapp.config.security.userdetails;
 
+import com.mfdev.redditcloneapp.exception.AccountNotFoundException;
 import com.mfdev.redditcloneapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +18,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return new UserDetailsImpl(
-                repository.findUserByUsername(username)
-                        .orElseThrow(() -> new RuntimeException("asdf"))
+                repository
+                        .findUserByUsername(username)
+                        .orElseThrow(() -> {
+                            String message = format("Account with username '%s' were not found", username);
+                            throw new AccountNotFoundException(message);
+                        })
         );
     }
 }
