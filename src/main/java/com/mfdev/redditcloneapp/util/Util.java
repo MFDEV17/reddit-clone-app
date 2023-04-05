c2530d17f58b2a400e5cf00be82acc563f32e324
mfdev17 <maximsushkin02@gmail.com>
2023-04-06 02:53:50 +0300

    added new method to util

--- src/main/java/com/mfdev/redditcloneapp/util/Util.java
+++ src/main/java/com/mfdev/redditcloneapp/util/Util.java
@@ -1,9 +1,12 @@
 package com.mfdev.redditcloneapp.util;
 
 import com.mfdev.redditcloneapp.dto.user.SignupDto;
+import com.mfdev.redditcloneapp.entity.user.User;
+import com.mfdev.redditcloneapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import static java.lang.String.format;
@@ -14,6 +17,8 @@ import static org.apache.commons.lang3.StringUtils.substringBetween;
 @RequiredArgsConstructor
 public class Util {
 
+    private final UserRepository userRepository;
+
     public ResponseEntity<String> onUniqueKeyException(DataIntegrityViolationException e) {
         String key = substringBetween(requireNonNull(e.getRootCause()).getMessage(), "Key (", ")");
         String val = substringBetween(requireNonNull(e.getRootCause()).getMessage(), "=(", ")");
@@ -23,5 +28,11 @@ public class Util {
 
     public String generateSuccessfulMessage(SignupDto dto) {
         return format("You're almost done! \nWe sent a verification mail to '%s'", dto.getEmail());
+    }
+
+    public User getSessionUser(Authentication auth) {
+        return userRepository
+                .findUserByUsername(auth.getName())
+                .orElseThrow();
     }
 }
